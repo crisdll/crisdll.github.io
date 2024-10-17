464e148a08d501ad669f9b094d00e5eb042ca425
crisdll <157112729+crisdll@users.noreply.github.com>
2024-10-17 17:13:45 +0200

    Update scriptFormulario.js

--- scriptFormulario.js
+++ scriptFormulario.js
@@ -69,7 +69,7 @@ function enviarFormulario() {
 
 // Realiza una solicitud AJAX para enviar los datos al script de Google Apps
 var xhr = new XMLHttpRequest();
-xhr.open('POST', 'https://script.google.com/macros/s/AKfycbzwM9g4YSMB_Xjqv8gBQI5l8bHUalxM2RIbrQGZG39zJIolhZE5kVee--bRF5_TYuBE/exec', true);
+xhr.open('POST', 'https://script.google.com/macros/s/AKfycbyybN4yqmnsbDDgbCrt_3A6RbouHLlFquE0RH7vQ4Gx_jX3BCqlHnCGJMlqwDcsiGVp/exec', true);
 xhr.setRequestHeader('Content-Type', 'application/json'); // Asegura que la solicitud sea enviada como JSON
 
 xhr.onload = function() {
